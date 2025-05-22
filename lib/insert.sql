eda7a09f0dbd17ffa29bea73ccb6527d68a0ef45
Cristina <kamuthucristina@gmail.com>
2025-05-22 12:20:04 +0300

    completed assignment

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES("Mr.Chocolate",23,"M","Brown","sad",0);
-INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES("Rowdy",15,"M","Black","Happy",1);
-INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES("Tabitha",20,"F","Brown","Mesmerised",0);
-INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES("Sergeant Brown",4,"M","Brown","sad",1);
-INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES("Melissa",18,"F","White","sad",1);
-INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES("Grinch",56,"M","Black","Excited",0);
-INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES("Wendy",23,"F","Brown","Gloomy",1);
-INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES(NULL,NULL,"M","Black","sad",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Mr.Chocolate",23,"M","Brown","sad",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Rowdy",15,"M","Black","Happy",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Tabitha",20,"F","Brown","Mesmerised",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Sergeant Brown",4,"M","Brown","sad",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Melissa",18,"F","White","sad",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Grinch",56,"M","Black","Excited",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Wendy",23,"F","Brown","Gloomy",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES(NULL,NULL,"M","Black","sad",1);
