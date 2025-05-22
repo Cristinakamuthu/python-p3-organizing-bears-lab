0494557e959e1b783276cd2c66659d665a6cb1f2
Cristina <kamuthucristina@gmail.com>
2025-05-22 12:05:22 +0300

    created and inserted

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES("Mr.Chocolate",23,"M","Brown","sad",0);
+INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES("Rowdy",15,"M","Black","Happy",1);
+INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES("Tabitha",20,"F","Brown","Mesmerised",0);
+INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES("Sergeant Brown",4,"M","Brown","sad",1);
+INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES("Melissa",18,"F","White","sad",1);
+INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES("Grinch",56,"M","Black","Excited",0);
+INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES("Wendy",23,"F","Brown","Gloomy",1);
+INSERT INTO bears (name,age,sex,color,temperement,alive) VALUES(NULL,NULL,"M","Black","sad",1);
